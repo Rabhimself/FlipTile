63aa00b7c955f540b1296b16cbcaff0cb90963fc
Robert Bryant <Robert Bryant>
2016-01-29 11:53:41 +0000

    Cleaned out bin folder

--- FlipTile/GamePage.xaml.cs
+++ FlipTile/GamePage.xaml.cs
@@ -33,17 +33,16 @@ namespace FlipTile
         {
             Rectangle r = (Rectangle)sender;
             Flip(r);
-            String name = r.Name;
-            
-            //not converting properly
-           int row = 2;
-           int col = 3;
+            String name = r.Name.Substring(1);
+            int row = Convert.ToInt16(name.Substring(0, 1));
+            int col = Convert.ToInt16(name.Substring(1));
+
             Grid g = (Grid)r.Parent;
-            Rectangle temp = (Rectangle)g.FindName("r" + (row - 1) + "" + col);
+           
             //down
-            if (!(row-1 < 0))
+            if (!(row - 1 < 0))
             {
-                Flip(temp);
+                Flip((Rectangle)g.FindName("r" + (row - 1) + "" + col));
             }
             //up
             if (!(row + 1 > 3))
@@ -51,22 +50,52 @@ namespace FlipTile
                 Flip((Rectangle)g.FindName("r" + (row + 1) + col));
             }
             //
-            if(!(col-1 < 0))
+            if (!(col - 1 < 0))
             {
-                Flip((Rectangle)g.FindName("r" + row + (col-1)));
+                Flip((Rectangle)g.FindName("r" + row + (col - 1)));
             }
             //down
-            if(!(col+1 > 5))
+            if (!(col + 1 > 5))
             {
-                Flip((Rectangle)g.FindName("r" + row  + (col+1)));
+                Flip((Rectangle)g.FindName("r" + row + (col + 1)));
             }
         }
 
+        //Works but is VERY hackey, as there is no way of stopping each animation once started
+        //either look into awaiting each storyboard, or possibly do one animation and bind each other affected
+        //tile's height&color.
         private void Flip(Rectangle r)
-        {
-            rectSB.Stop();
-            Storyboard.SetTarget(rectSB, r);
-            rectSB.Begin();
+        {        
+            Storyboard sb = new Storyboard();
+
+            DoubleAnimation da = new DoubleAnimation();
+            da.AutoReverse = true;
+            da.EnableDependentAnimation = true;
+            da.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 120));
+            da.From = 200;
+            da.To = 0;
+
+            ColorAnimation ca = new ColorAnimation();
+            ca.AutoReverse = false;
+            ca.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 60));
+
+            SolidColorBrush faceDownColor = new SolidColorBrush(Windows.UI.Colors.AliceBlue);
+            SolidColorBrush currentColor = (SolidColorBrush)r.Fill;
+
+            if (currentColor.Color == faceDownColor.Color)
+                ca.To = (Windows.UI.Colors.Blue);
+            else
+                ca.To = (Windows.UI.Colors.AliceBlue);
+
+            Storyboard.SetTargetProperty(da, "Height");
+            Storyboard.SetTargetProperty(ca, "(Fill).(Color)");
+            Storyboard.SetTarget(ca, r);
+            Storyboard.SetTarget(da, r);
+            
+            sb.Children.Add(da);
+            sb.Children.Add(ca);
+
+            sb.Begin();
         }
     }
 }
