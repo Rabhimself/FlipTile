945a22ce5a5a12a17012ef897d750df407e800b6
Robert Bryant <rabhimself86@gmail.com>
2016-02-12 18:03:59 +0000

    Added some randomization

--- FlipTile/GamePage.xaml.cs
+++ FlipTile/GamePage.xaml.cs
@@ -24,78 +24,115 @@ namespace FlipTile
     /// </summary>
     public sealed partial class GamePage : Page
     {
+        private Boolean animating = false;
+        private int winFlag = 24;
+        private Rectangle r;
+
         public GamePage()
         {
             this.InitializeComponent();
+            Randomize();
         }
 
         private void tapped(object sender, TappedRoutedEventArgs e)
         {
-            Rectangle r = (Rectangle)sender;
-            Flip(r);
-            String name = r.Name.Substring(1);
-            int row = Convert.ToInt16(name.Substring(0, 1));
-            int col = Convert.ToInt16(name.Substring(1));
+            if (!animating)
+            {
+                animating = true;
+                r = (Rectangle)sender;
+                Flip(r);
+                String name = r.Name.Substring(1);
+                int row = Convert.ToInt16(name.Substring(0, 1));
+                int col = Convert.ToInt16(name.Substring(1));
 
-            Grid g = (Grid)r.Parent;
-           
-            //down
-            if (!(row - 1 < 0))
-            {
-                Flip((Rectangle)g.FindName("r" + (row - 1) + "" + col));
+                Grid g = (Grid)r.Parent;
+
+                //down
+                if (!(row - 1 < 0))
+                {
+                    Flip((Rectangle)g.FindName("r" + (row - 1) + "" + col));
+                }
+                //up
+                if (!(row + 1 > 3))
+                {
+                    Flip((Rectangle)g.FindName("r" + (row + 1) + col));
+                }
+                //
+                if (!(col - 1 < 0))
+                {
+                    Flip((Rectangle)g.FindName("r" + row + (col - 1)));
+                }
+                //down
+                if (!(col + 1 > 5))
+                {
+                    Flip((Rectangle)g.FindName("r" + row + (col + 1)));
+                }
             }
-            //up
-            if (!(row + 1 > 3))
-            {
-                Flip((Rectangle)g.FindName("r" + (row + 1) + col));
-            }
-            //
-            if (!(col - 1 < 0))
-            {
-                Flip((Rectangle)g.FindName("r" + row + (col - 1)));
-            }
-            //down
-            if (!(col + 1 > 5))
-            {
-                Flip((Rectangle)g.FindName("r" + row + (col + 1)));
-            }
+            
         }
 
-        //Works but is VERY hackey, as there is no way of stopping each animation once started
-        //either look into awaiting each storyboard, or possibly do one animation and bind each other affected
-        //tile's height&color.
         private void Flip(Rectangle r)
-        {        
+        {   
+                 
+            //Need to replace this method of animating multiple rectangles.
+            //Too many Storyboard objects are being GCed
             Storyboard sb = new Storyboard();
 
             DoubleAnimation da = new DoubleAnimation();
             da.AutoReverse = true;
             da.EnableDependentAnimation = true;
-            da.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 120));
-            da.From = 200;
+            da.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 250));
             da.To = 0;
 
             ColorAnimation ca = new ColorAnimation();
             ca.AutoReverse = false;
-            ca.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 60));
+            ca.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 1));
+            ca.BeginTime = new TimeSpan(0, 0, 0, 0, 125);
 
             SolidColorBrush faceDownColor = new SolidColorBrush(Windows.UI.Colors.AliceBlue);
             SolidColorBrush currentColor = (SolidColorBrush)r.Fill;
 
             if (currentColor.Color == faceDownColor.Color)
+            {
                 ca.To = (Windows.UI.Colors.Blue);
+                winFlag++;
+            }
             else
+            {
                 ca.To = (Windows.UI.Colors.AliceBlue);
+                winFlag--;
+            }           
 
             Storyboard.SetTargetProperty(da, "Height");
             Storyboard.SetTargetProperty(ca, "(Fill).(Color)");
             Storyboard.SetTarget(ca, r);
             Storyboard.SetTarget(da, r);
-            
+            sb.Duration = new Duration(new TimeSpan(0, 0, 0, 0, 500));
             sb.Children.Add(da);
             sb.Children.Add(ca);
+            sb.Completed += new EventHandler<object>(ToggleAnim);
 
             sb.Begin();
+        }
+
+        private void ToggleAnim(object sender, object e)
+        {    
+            animating = false;
+            if (winFlag == 0)
+                Frame.Navigate(typeof(MainPage));
+        }
+        private void Randomize()
+        {
+            Random rand = new Random();
+            int max = rand.Next(12);
+
+            for(int i = 0; i< max; i++)
+            {
+                int row = rand.Next(3);
+                int col = rand.Next(5);
+
+                Flip((Rectangle)FindName("r" + row +"" + col));
+            }
         }
     }
 }
