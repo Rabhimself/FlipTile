42dd803e9c547636425807224655db620da6e574
Robert Bryant <Robert Bryant>
2016-03-19 15:54:49 +0000

    Reset Button Implemented

--- FlipTile/GamePage.xaml.cs
+++ FlipTile/GamePage.xaml.cs
@@ -14,27 +14,32 @@ using Windows.UI.Xaml.Shapes;
 
 namespace FlipTile
 {
-    //TODO:
+    //TODO: Figure out why the ui becomes unresponsive at times, for about 1sec. Tends to coincide with GC, reduction of local variables didn't help much
     public sealed partial class GamePage : Page
     {
+        //global variable for checking if an animation is running
         private Boolean animating = false;
-        private int winFlag = 24;
-        private Tile tile;
-        SolidColorBrush brush;
+        //Use a grid to ref tiles, reduce findname calls
+        private static int rows = 4, cols = 6, winFlag = 24;
+        private Tile[,] tileGrid = new Tile[rows, cols];
         //
-        private Tile[,] tileGrid = new Tile[4, 6];
-        //
+
         public GamePage()
         {
             this.InitializeComponent();
             Init();
-            Loaded += Randomize;
+            Loaded += RandomizeOnLoad;
             
         }
 
-        private async void Randomize(object sender, RoutedEventArgs e)
+        private void RandomizeOnLoad(object sender, RoutedEventArgs e)
         {
+            Randomize();
+        }
 
+        //Picks 2-12 tiles and flips them, runs once the page is loaded to add some degree of difficulty
+        private async void Randomize()
+        {
 
             Random rand = new Random();
             int max = rand.Next(2, 12);
@@ -44,10 +49,13 @@ namespace FlipTile
                 int row = rand.Next(3);
                 int col = rand.Next(5);
 
+                //wait for the previous animation to finish
                 while (animating)
                 {
+                    //poll every 25ms
                     await Task.Delay(25);
                 }
+
                 Flip(tileGrid[row, col], rectTappedSB);
             }
 
@@ -57,9 +65,13 @@ namespace FlipTile
         {
             if (!animating)
             {
+                //get the target of the storyboard
+                char[] name = ((Rectangle)sender).Name.ToCharArray();
 
-                int row = Convert.ToInt16(((Rectangle)sender).Name.Substring(1, 1));
-                int col = Convert.ToInt16(((Rectangle)sender).Name.Substring(2));
+                //row and column are stored in the name
+                int row = Convert.ToInt16(name[1].ToString());
+                int col = Convert.ToInt16(name[2].ToString());
+
                 Flip(tileGrid[row, col], rectTappedSB);
 
                 //down
@@ -77,7 +89,7 @@ namespace FlipTile
                 {
                     Flip(tileGrid[row, col - 1], rectLeftSB);
                 }
-                //down
+                //right
                 if (!(col + 1 > 5))
                 {
                     Flip(tileGrid[row, col + 1], rectRightSB);
@@ -95,40 +107,51 @@ namespace FlipTile
 
             if (tile.Faceup)
             {
+                //getting a color animation from a storyboard *could* be easier...
                 sb.Children.OfType<ColorAnimation>().First().To = Windows.UI.Colors.Blue;
             }
             else
             {
                 sb.Children.OfType<ColorAnimation>().First().To = Windows.UI.Colors.AliceBlue;
             }
+            //Override the stretch with an actual height to allow the storyboard to run
             tile.Rect.Height = tile.Rect.ActualHeight;
             sb.Begin();
         }
 
         private void ToggleAnim(object sender, object e)
         {
+            //get the target of the storyboard
             char[] name = Storyboard.GetTargetName(((Storyboard)sender)).ToCharArray();
 
+            //row and column are stored in the name
             int row = Convert.ToInt16(name[1].ToString());
             int col = Convert.ToInt16(name[2].ToString());
 
+            //determine if the tile is faceup, and set the approriate color
+            //this prevents the color from reverting once the storyboard stops
             if (tileGrid[row, col].Faceup)
             {
+                //Cast is necessary to change the color of the rectangle's fill property
                 ((SolidColorBrush)tileGrid[row, col].Rect.Fill).Color = Windows.UI.Colors.Blue;
             }
             else
             {
+                //so many parenthesis
                 ((SolidColorBrush)tileGrid[row, col].Rect.Fill).Color = Windows.UI.Colors.AliceBlue;
             }
 
+            //using yet another cast to reduce local variables on the heap
             ((Storyboard)sender).Stop();
 
             tileGrid[row, col].Faceup = !tileGrid[row, col].Faceup;
             animating = false;
+
             if (winFlag == 0)
                 Frame.Navigate(typeof(MainPage));
         }
 
+        //sets up the tilegrid for use later on
         private void Init()
         {
             for (int row = 0; row < 4; row++)
@@ -141,6 +164,34 @@ namespace FlipTile
                     tileGrid[row, col] = tile;
                 }
             }
+        }
+
+        private void Pause_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        //Flips everything over, then randomizes the board
+        private async void Reset_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    if (tileGrid[j, i].Faceup == true)
+                    {
+                        //wait for the previous animation to finish
+                        while (animating)
+                        {
+                            //poll every 25ms
+                            await Task.Delay(25);
+                        }
+                        Flip(tileGrid[j, i], rectTappedSB);
+                    }
+                }
+            }
+
+            Randomize();
         }
     }
 }
